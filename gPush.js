24a95b22ba9d710c8980b365b478b11a738be84f
valcoellar <valcoellar@gmail.com>
2023-09-15 14:57:12 -0600

    Add: options

--- gPush.js
+++ gPush.js
@@ -9,11 +9,14 @@
 const readline = require('readline');   
 const { exec } = require('child_process');  // to execute in cli
 
+const now = new Date().toLocaleString();
+let commitMsg = now;
+
 function menu(){
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+	const rl = readline.createInterface({
+	  input: process.stdin,
+	  output: process.stdout
+	});
 
 
 function runCommands(comm){
@@ -34,7 +37,7 @@ function runCommands(comm){
 }
 
 
-rl.question('Select an option:\n1. Select work dir \n2. Change msg  \n3. show Status\n4. Add . all\n5. Fast Push \n<--. Exit Press CTRL + c\n', (respuesta) => {
+rl.question('Select an option:\n1. Select work dir \n2. Change msg  \n3. show Status\n4. Add . all\n5. Fast Push \n7. Store Credentials \n<--. Exit Press CTRL + c\n', (respuesta) => {
   switch (respuesta) {
     case '1':
       console.log('\nOpción 1 seleccionada\n');
@@ -53,9 +56,15 @@ rl.question('Select an option:\n1. Select work dir \n2. Change msg  \n3. show St
 	break;
 	case '5':
 	      console.log('\n4-Executing git add. // commit -m "date" // push \n');
-				//runCommands("git add .")
+		//runCommands("git add .")
+		//runCommands("git commit -m " + "'" + commitMsg + "'")
+		runCommands("git push ");
 	break;
 	
+	case '7':
+		console.log("\n7-Executing  git config --global credential.helper store \n")
+		runCommands("git config --global credential.helper store");
+	break;
 
     default:
       console.log('\nNot an Option\n');
